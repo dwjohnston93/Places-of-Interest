3b6bd8dbbfb8bd910790726b55745cb32670fa1b
dwjohnston93 <dwjohnston93@gmail.com>
2018-06-20 19:03:04 -0700

    Google Maps and geolocation work, and manual marker adds to the center of the maps

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -40,5 +40,31 @@ export class HomePage {
     });
  
   }
+
+  addInfoWindow(marker, content){
+ 
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+   
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+   
+  }
+
+  addMarker(){
+ 
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+   
+    let content = "<h4>Information!</h4>";         
+   
+    this.addInfoWindow(marker, content);
+   
+  }
  
 }
