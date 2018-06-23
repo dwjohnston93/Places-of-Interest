29bf34943add17dc4805a00cf1e1d1c572ba991f
dwjohnston93 <dwjohnston93@gmail.com>
2018-06-23 11:52:11 -0700

    login works, error alert message appears on failed login attempt

--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AppUserProvider } from '../../providers/app-user/app-user';
 import { HomePage } from '../../pages/home/home';
+import { AlertController } from 'ionic-angular';
 
 
 /**
@@ -17,22 +18,38 @@ import { HomePage } from '../../pages/home/home';
 })
 export class SignInPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public _appUser: AppUserProvider) {
-  }
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public _appUser: AppUserProvider,
+    public alertCtrl: AlertController
+  ) {}
 
   user = {};
   newUser = {};
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SignInPage');
+    console.log('ionViewDidLoad SignInPage')
   }
+
+  showAlert(param) {   
+    const alert = this.alertCtrl.create({
+      title: param,
+      subTitle: '',
+      buttons: ['OK']
+    });
+    alert.present();
+}
 
   doLogin(){
     this._appUser.userLogin(this.user)
     .subscribe((data:any) => {
       console.log("doLogin on sign-in.ts")
       this.navCtrl.push(HomePage);
-      }, err => {console.log("login failed")});
+      }, err => {
+        this.showAlert("login failed");
+        console.log("login failed")
+      });
     
     }
 
