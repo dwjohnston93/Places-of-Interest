e2ca51b93d8843e01b26370f65d65be1bdc1f4fc
dwjohnston93 <dwjohnston93@gmail.com>
2018-06-25 19:30:28 -0700

    markers show place name when clicked

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -26,8 +26,7 @@ export class HomePage {
   loadMap(){
 
     let that = this;
-    console.log(that, "that"); 
- 
+
     this.geolocation.getCurrentPosition().then((position) => {
  
       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
@@ -54,8 +53,6 @@ export class HomePage {
       function callback(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
           for (var i = 0; i < results.length; i++) {
-            console.log("results:", results);
-            console.log(this, "this")
             that.createMarker(results[i]);
           }
         }
@@ -68,54 +65,14 @@ export class HomePage {
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
       map: this.map,
-      position: place.geometry.location
+      position: placeLoc
     });
   
-    google.maps.event.addListener(marker, 'click', function() {
+    google.maps.event.addListener(marker, 'click', _ => {
       this.infowindow.setContent(place.name);
-      this.infowindow.open(this.map, this);
+      this.infowindow.open(this.map, marker);
     });
 }
-
-  
-
-  // function initMap() {
-  //   var pyrmont = {lat: -33.867, lng: 151.195};
-
-  //   map = new google.maps.Map(document.getElementById('map'), {
-  //     center: pyrmont,
-  //     zoom: 15
-  //   });
-
-  //   infowindow = new google.maps.InfoWindow();
-  //   var service = new google.maps.places.PlacesService(map);
-  //   service.nearbySearch({
-  //     location: pyrmont,
-  //     radius: 500,
-  //     type: ['store']
-  //   }, callback);
-  // }
-
-  // function callback(results, status) {
-  //   if (status === google.maps.places.PlacesServiceStatus.OK) {
-  //     for (var i = 0; i < results.length; i++) {
-  //       createMarker(results[i]);
-  //     }
-  //   }
-  // }
-
-  // function createMarker(place) {
-  //   var placeLoc = place.geometry.location;
-  //   var marker = new google.maps.Marker({
-  //     map: map,
-  //     position: place.geometry.location
-  //   });
-
-  //   google.maps.event.addListener(marker, 'click', function() {
-  //     infowindow.setContent(place.name);
-  //     infowindow.open(map, this);
-  //   });
-  // }
 
   addInfoWindow(marker, content){
  
